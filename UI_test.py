d6b48c6430f6ec64faa7d1502238c7d83767393d
LE MENTEC Alexandre <alemente@silure.enseeiht.fr>
2024-10-11 17:58:41 +0200

    Adding some buttons on the calc part

--- UI_test.py
+++ UI_test.py
@@ -44,6 +44,8 @@ class PIV_UI :
     def refresh_window(self):
         self.image.refresh()
         self.image.type_choice()
+        self.calcul.refresh()
+        self.calcul.choice()
 
     def main_loop(self):
         self.window.after(100,self.refresh_window)
@@ -58,24 +60,71 @@ class calcul_input:
         #fonts 
         self.text_font = font.Font(family='Consolas',size=10)
         self.title_font = font.Font(family='Consolas',size=20)
+        #Styles
+        self.deroulant_style = ttk.Style()
+        self.deroulant_style.theme_use('clam')
+        self.deroulant_style.configure("TCombobox", fieldbackground= "gray3", background= "gray3", foreground="chartreuse2",
+                                        bordercolor="chartreuse2",arrowcolor="chartreuse2",
+                                        lightcolor="chartreuse2",darkcolor="gray3",focusfill="gray3",
+                                        selectbackground="gray3", selectforeground="chartreuse2")
         #Variables
+            #method choice
         self.CalculCPIV_meths = '' 
+        self.CalculCPIV_meths_list = ['IVDEFORM ','PIVDECAL ','PIVSIMPLE']
+            #X and Y
+        self.CalculCPIV_dimXYcell = ''
+        self.CalculCPIV_dimX = ''
+        self.CalculCPIV_dimY = ''
+
         #global interactive items
+            # method choice
+        self.choice_CalculCPIV_meths = ttk.Combobox(self.calc_frame,values=self.CalculCPIV_meths_list)
+            # X and Y
+        self.inp_CalculCPIV_dimX = tk.Entry(self.calc_frame,foreground="chartreuse2",background="gray3",
+                                   highlightcolor="chartreuse2",highlightbackground="chartreuse2",insertbackground="chartreuse2",font=self.text_font,width=9)
+        self.inp_CalculCPIV_dimY = tk.Entry(self.calc_frame,foreground="chartreuse2",background="gray3",
+                                   highlightcolor="chartreuse2",highlightbackground="chartreuse2",insertbackground="chartreuse2",font=self.text_font,width=9)
 
         #Toggleable non-interactive items
 
+
         #Non-toggleable non-interactive items
+            #global name
         self.Name = tk.Label(self.calc_frame, text='> CALCUL')
+        self.Name.config(bg='gray3',fg='chartreuse2',font=self.title_font)
+            # Method choice
+        self.meth_choice_label = tk.Label(self.calc_frame, text='>> Chosen computing method')
+        self.meth_choice_label.config(bg='gray3',fg='chartreuse2',font=self.text_font)
+            # X and Y
+        self.XY_dim_label = tk.Label(self.calc_frame, text='>> X and Y size of the comp window')
+        self.XY_dim_label.config(bg='gray3',fg='chartreuse2',font=self.text_font)
+        self.XY_dim_choice_label = tk.Label(self.calc_frame, text='>>> [X][Y]')
+        self.XY_dim_choice_label.config(bg='gray3',fg='chartreuse2',font=self.text_font)
+
 
     def setup(self):
         #setup the frame
         self.calc_frame.config(bg='gray3',highlightbackground='chartreuse2',highlightcolor="chartreuse2",highlightthickness=1,relief='ridge')
         self.calc_frame.pack(fill='x', padx = 5, pady = 2)
         # put the name of the frame
-        self.Name.config(bg='gray3',fg='chartreuse2',font=self.title_font)
         self.Name.grid(column = 0, row= 0)
+        # Method choice
+        self.meth_choice_label.grid(column = 1, row= 1, sticky = 'W')
+        self.choice_CalculCPIV_meths.grid(column = 2, row= 1, sticky = 'W')
+        # XY choice
+        self.XY_dim_label.grid(column = 1, row= 2, sticky = 'W')
+        self.XY_dim_choice_label.grid(column = 2, row= 3, sticky = 'W')
+        self.inp_CalculCPIV_dimX.grid(column = 3, row= 3, sticky = 'E')
+        self.inp_CalculCPIV_dimY.grid(column = 4, row= 3, sticky = 'W')
 
     def refresh(self):
+        # method choice
+        self.CalculCPIV_meths = self.choice_CalculCPIV_meths.get()
+        # X Y
+
+
+
+    def choice(self):
         pass
 
 class action_input:
